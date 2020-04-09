a72a3afd9558055115d5ea3d32101939152a1186
FishIsMyNick <luxlupus@gmail.com>
2020-04-09 17:09:45 +0200

    Prikaz za neregistrovanog korisnika, neke klase u stylesheet-u

--- Travel-bois/src/app/app.module.ts
+++ Travel-bois/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NeregistrovanKorisnikComponent } from './neregistrovan-korisnik/neregistrovan-korisnik.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NeregistrovanKorisnikComponent
   ],
   imports: [
     BrowserModule,
---
+++ Travel-bois/src/app/neregistrovan-korisnik/neregistrovan-korisnik.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-neregistrovan-korisnik',
+  templateUrl: './neregistrovan-korisnik.component.html',
+  styleUrls: ['./neregistrovan-korisnik.component.css']
+})
+export class NeregistrovanKorisnikComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
