2ba0716282602b63d7566a6a73c7c666062adc02
tasanaMitic <dmitic21@yahoo.com>
2020-04-11 23:59:52 +0200

    odradjena registracija admina rent-a-car i avio kompanije

--- Travel-bois/src/app/reg-admina-avio/reg-admina-avio.component.ts
+++ Travel-bois/src/app/reg-admina-avio/reg-admina-avio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reg-admina-avio',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reg-admina-avio.component.css']
 })
 export class RegAdminaAvioComponent implements OnInit {
-
+  regAvioAdminForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  private initForm()
+  {
+    this.regAvioAdminForm = new FormGroup({
+      'ime': new FormControl('ime',[Validators.required, Validators.maxLength(15), Validators.minLength(3)]),
+      'prezime': new FormControl('pr', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+      'grad': new FormControl('grad', [Validators.required, Validators.maxLength(10), Validators.minLength(3)]),
+      'avioKompanija': new FormControl('avioKompanija',Validators.required),    //dodati posle da se ucitava jedna od postojecih aviokompanija
+      'brojTelefona': new FormControl('0', Validators.required),
+      'brojPasosa': new FormControl('0', Validators.required),
+      'username': new FormControl('user', Validators.required),
+      'password': new FormControl('pass', Validators.required),
+    });
+  }
+
+  onSubmit(){
+    console.log(this.regAvioAdminForm.value);
+    console.log(this.regAvioAdminForm);
+  }
 }
--- Travel-bois/src/app/reg-admina-rente/reg-admina-rente.component.ts
+++ Travel-bois/src/app/reg-admina-rente/reg-admina-rente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reg-admina-rente',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reg-admina-rente.component.css']
 })
 export class RegAdminaRenteComponent implements OnInit {
-
+  regRenteAdminForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm()
+  {
+    this.regRenteAdminForm = new FormGroup({
+      'ime': new FormControl('ime',[Validators.required,Validators.maxLength(15), Validators.minLength(3)]),
+      'prezime': new FormControl('pr', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+      'grad': new FormControl('grad', [Validators.required, Validators.maxLength(10), Validators.minLength(3)]),
+      'rentaKompanija': new FormControl('rentaKompanija',Validators.required),    //dodati posle da se ucitava jedna od postojecih aviokompanija
+      'brojTelefona': new FormControl('0', Validators.required),
+      'brojPasosa': new FormControl('0', Validators.required),
+      'username': new FormControl('user', Validators.required),
+      'password': new FormControl('pass', Validators.required),
+    });
+  }
+
+  onSubmit(){
+    console.log(this.regRenteAdminForm.value);
+    console.log(this.regRenteAdminForm);
   }
 
 }
