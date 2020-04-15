29b0fd89d59f3a0bcd9c6e439d151b8267e849c5
FishIsMyNick <luxlupus@gmail.com>
2020-04-15 11:11:43 +0200

    Ispravljena greska kod prikaza neregistrovanom korisniku
    Tabela funkcija je bila malo cudna

--- Travel-bois/src/app/app.component.ts
+++ Travel-bois/src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent {
     //AppComponent.currentUser = new Admin('sysAdmin', 'password');
     //AppComponent.currentUser = new Admin();
     AppComponent.currentUser = new User();
-    //AppComponent.currentUser = new RegisteredUser('060123456', 'Novi Sad', 'Pera', 'Zdera', 'prozdera', 'password', 111546);
+    // AppComponent.currentUser = new RegisteredUser('060123456', 'Novi Sad', 'Pera', 'Zdera', 'prozdera', 'password', 111546);
     //AppComponent.currentUser = new AvioAdmin('060123456', 'Novi Sad', 'Pera', 'Zdera', 'prozdera', 'password');
     AppComponent.datum = new Date();
     
