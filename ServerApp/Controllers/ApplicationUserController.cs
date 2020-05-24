34cae7d7e38f696df089d20f20424a1e7162a7bc
FishIsMyNick <luxlupus@gmail.com>
2020-05-24 12:13:02 +0200

    Nisam siguran sta

---
+++ ServerApp/Controllers/ApplicationUserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+
+namespace ServerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userMngr, SignInManager<ApplicationUser> signInMngr)
+        {
+            _userManager = userMngr;
+            _signInManager = signInMngr;
+        }
+
+        [HttpGet]
+    }
+}
