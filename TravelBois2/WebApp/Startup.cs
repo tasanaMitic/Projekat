598824bc7e2ab696fcac2417b9f5d4fb0ecc1b99
FishIsMyNick <luxlupus@gmail.com>
2020-05-25 03:36:44 +0200

    Ne valja

--- TravelBois2/WebApp/Startup.cs
+++ TravelBois2/WebApp/Startup.cs
@@ -41,6 +41,8 @@ namespace WebApp
                 options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequireUppercase = false;
             });
+
+            //services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +58,11 @@ namespace WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //app.UseCors(builder =>
+            //builder.WithOrigins("https://localhost:44328")
+            //.AllowAnyHeader()
+            //.AllowAnyMethod());
 
             app.UseAuthentication();
 
