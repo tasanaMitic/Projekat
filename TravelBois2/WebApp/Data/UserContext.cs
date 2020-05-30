62dd2b5a632485e41165b7e083f63f9b3daf7e11
FishIsMyNick <luxlupus@gmail.com>
2020-05-30 21:46:42 +0200

    Zapocet rad na contextima

---
+++ TravelBois2/WebApp/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+	public class UserContext : DbContext
+	{
+		public UserContext(DbContextOptions<UserContext> options) : base(options)
+		{ }
+		public DbSet<User> Users { get; set; }
+		public DbSet<RentAdmin> RentAdmins { get; set; }
+		public DbSet<AvioAdmin> AvioAdmins { get; set; }
+		public DbSet<Admin> Admins { get; set; }
+	}
+}
